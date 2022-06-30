884785f33429c25c12b4fea83c15d47253609e33
kkw855 <kkw855@gmail.com>
2022-06-30 17:07:13 +0900

    첫 번째 커밋

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+num1 = 25000000
+num2 = 25_000_000
+print(num1)
+print(num2)
+print(int('25_000_000'))
+
+float1 = 25_000_000.0
+float2 = 25e6
+print(float1)
+print(float2)
+print(31415926e-7)
